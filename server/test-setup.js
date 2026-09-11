0e5dcccb379a9f4ea55324b21515f2d9169e6623
abhiraj <abhirajmait16@gmail.com>
2026-09-12 04:37:28 +0530

    clean up

--- server/test-setup.js
+++ server/test-setup.js
@@ -8,8 +8,8 @@ if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET is not configured");
 }
 
-if (!process.env.JWT_REFRESH_SECRET) {
-  throw new Error("JWT_REFRESH_SECRET is not configured");
+if (!process.env.REFRESH_TOKEN_SECRET) {
+  throw new Error("REFRESH_TOKEN_SECRET is not configured");
 }
 
 if (!process.env.DATABASE_URL) {
